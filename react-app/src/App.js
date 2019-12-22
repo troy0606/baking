e8a89fe025f6d6ef23ac5ec52ecb843a5b019165
BoAn Chen <troy0606troy0606@gmail.com>
2019-12-22 18:30:16 +0800

    會員session 登入會員流水號 名稱 圖片存狀態ok

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -18,10 +18,10 @@ import {
   Switch,
   Redirect
 } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/Store";
+// import { Provider } from "react-redux";
+// import store from "./redux/Store";
 import { useDispatch } from "react-redux";
-import { LOG_IN_ACTION } from "./pages/member/Actions";
+import { LOG_IN_ACTION, LOG_OUT_ACTION } from "./pages/member/Actions";
 
 function App() {
   const [orderCart, setOrderCart] = useState([]);
@@ -33,45 +33,50 @@ function App() {
         withCredentials: true
       })
       .then(result => {
-        console.log(result);
         if (result.data.status === "202") {
           return result.data.data;
         }
       })
       .then(result => {
-        dispatch(LOG_IN_ACTION());
-        console.log(result);
+        if (result) {
+          let memberInfo = {
+            memberSid: result.member_sid,
+            memberName: result.member_name,
+            memberPic: result.member_picture
+          };
+          dispatch(LOG_IN_ACTION(memberInfo));
+        } else {
+          dispatch(LOG_OUT_ACTION());
+        }
       })
       .catch();
     //eslint-disable-next-line
   }, []);
   return (
     <>
-      <Provider store={store}>
-        <Router history={BrowserRouter}>
-          <NavBar />
-          <Switch>
-            <Route path="/home" exact component={Home}></Route>
-            <Route path="/" exact component={Navigation}></Route>
-            <Route
-              path="/member/signup_Login"
-              exact
-              component={MemberLogIn_SignUp}
-            ></Route>
-            <Route path="/member/:page?" exact component={Member}></Route>
-            <Route path="/products" exact component={Products}></Route>
-            <Route path="/teacher" exact component={Teacher}></Route>
-            <Route path="/forum" exact component={Forum}></Route>
-            <Route
-              path="/cart"
-              exact
-              component={() => <Cart orderCart={orderCart} />}
-            ></Route>
-            <Route path="" component={() => 404} />
-          </Switch>
-          <Footer />
-        </Router>
-      </Provider>
+      <Router history={BrowserRouter}>
+        <NavBar />
+        <Switch>
+          <Route path="/home" exact component={Home}></Route>
+          <Route path="/" exact component={Navigation}></Route>
+          <Route
+            path="/member/signup_Login"
+            exact
+            component={MemberLogIn_SignUp}
+          ></Route>
+          <Route path="/member/:page?" exact component={Member}></Route>
+          <Route path="/products" exact component={Products}></Route>
+          <Route path="/teacher" exact component={Teacher}></Route>
+          <Route path="/forum" exact component={Forum}></Route>
+          <Route
+            path="/cart"
+            exact
+            component={() => <Cart orderCart={orderCart} />}
+          ></Route>
+          <Route path="" component={() => 404} />
+        </Switch>
+        <Footer />
+      </Router>
     </>
   );
 }
