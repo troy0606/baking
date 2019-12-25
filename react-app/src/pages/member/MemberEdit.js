e4589eaee21bb2c9e94b25bc454b1006a7400e97
BoAn Chen <troy0606troy0606@gmail.com>
2019-12-25 14:06:45 +0800

    登入登出修改 會員中心略排版

---
+++ react-app/src/pages/member/MemberEdit.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import "./scss/member.scss";
+import { Link, Route, Switch } from "react-router-dom";
+
+function MemberEdit() {
+  useEffect(() => {}, []);
+  return (
+    <>
+      <h1>Hello Edit</h1>
+    </>
+  );
+}
+
+export default MemberEdit;
