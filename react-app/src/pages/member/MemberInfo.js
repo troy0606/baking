ec08377b2021728781c6cc0ce44b121d3c21248b
BoAn Chen <troy0606troy0606@gmail.com>
2020-01-03 16:52:13 +0800

    會員中心修改狀態

--- react-app/src/pages/member/MemberInfo.js
+++ react-app/src/pages/member/MemberInfo.js
@@ -3,7 +3,7 @@ import "./scss/member.scss";
 import { Link, Route, Switch } from "react-router-dom";
 import axios from "axios";
 import { GiRoundStar } from "react-icons/gi";
-import { GoTriangleRight,  GoTriangleLeft} from "react-icons/go";
+import { GoTriangleRight, GoTriangleLeft } from "react-icons/go";
 
 function MemberInfo() {
   const [step, setStep] = useState(false);
@@ -14,32 +14,88 @@ function MemberInfo() {
   const [userAddress, setUserAddress] = useState("");
   const [userLevel, setUserLevel] = useState(0);
   useEffect(() => {
-    axios
-      .get("http://localhost:5000/member/checklogin", {
-        withCredentials: true
-      })
-      .then(result => {
-        if (result.data.status === "202") {
+    if (!step) {
+      axios
+        .get("http://localhost:5000/member/checklogin", {
+          withCredentials: true
+        })
+        .then(result => {
           setUserEmail(result.data.data.member_email);
           setUserName(result.data.data.member_name);
-          let date = result.data.data.member_birth.split("T")[0];
-          setUserBirth(date);
+          setUserBirth(result.data.data.member_birth);
           setUserPhone(result.data.data.member_phone);
           setUserAddress(result.data.data.member_address);
           setUserLevel(result.data.data.member_level);
-        } else {
-          alert(result.data.message);
-          window.history.go(-1);
+          if (result.data.status === "202") {
+            console.log(userName);
+          } else {
+            alert(result.data.message);
+            window.history.go(-1);
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+    //eslint-disable-next-line
+  }, [step]);
+
+  const changeValue = e => {
+    switch (e.target.name) {
+      case "userEmail": {
+        setUserEmail(e.target.value);
+        break;
+      }
+      case "userName": {
+        setUserName(e.target.value);
+        break;
+      }
+      case "userBirth": {
+        setUserBirth(e.target.value);
+        break;
+      }
+      case "userPhone": {
+        setUserPhone(e.target.value);
+        break;
+      }
+      case "userAddress": {
+        setUserAddress(e.target.value);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
+  const submitChange = (
+    userEmail,
+    userName,
+    userBirth,
+    userPhone,
+    userAddress
+  ) => {
+    axios
+      .post(
+        "http://localhost:5000/member/changeInfo",
+        {
+          userEmail,
+          userName,
+          userBirth,
+          userPhone,
+          userAddress
+        },
+        {
+          withCredentials: true
         }
+      )
+      .then(result => {
+        console.log(result);
       })
       .catch(error => {
         console.log(error);
       });
-  }, []);
-
-  const changeValue = e =>{
-    console.log(e.target.name)
-  }
+  };
   let levelArr = [];
   for (let i = 0; i < userLevel; i++) {
     //記得在JSX中使用JS變數要用花括號包著
@@ -47,6 +103,7 @@ function MemberInfo() {
       <GiRoundStar style={{ color: "#de726b", fontSize: "18px" }} />
     );
   }
+
   return (
     <>
       <div className="container px-4">
@@ -55,42 +112,59 @@ function MemberInfo() {
           <div>{levelArr}</div>
           <div className="d-flex">
             <div
-              onClick={!step?() => {
-                setStep(true);
-              }:''}
-            >{!step?
-            (
-              <>
-              <h5>修改</h5>
-              <GoTriangleRight
-                style={{
-                  fontSize: "36px",
-                  color: "#eca061",
-                  border: "none"
-                }}/>
-              </>
-              ):(
+              onClick={
+                !step
+                  ? () => {
+                      setStep(true);
+                    }
+                  : ""
+              }
+            >
+              {!step ? (
                 <>
-                  <div>
-                  <GoTriangleLeft
-                style={{
-                  fontSize: "36px",
-                  color: "#eca061",
-                  border: "none"
-                }}/>
-                <h5>取消</h5>
+                  <h5>修改</h5>
+                  <GoTriangleRight
+                    style={{
+                      fontSize: "36px",
+                      color: "#eca061",
+                      border: "none"
+                    }}
+                  />
+                </>
+              ) : (
+                <>
+                  <div onClick={() => setStep(false)}>
+                    <GoTriangleLeft
+                      style={{
+                        fontSize: "36px",
+                        color: "#eca061",
+                        border: "none"
+                      }}
+                    />
+                    <h5>取消</h5>
                   </div>
-                  <div>
-                  <h5>確定</h5>
-              <GoTriangleRight
-                style={{
-                  fontSize: "36px",
-                  color: "#eca061",
-                  border: "none"
-                }}/>
+                  <div
+                    onClick={() =>
+                      submitChange(
+                        userEmail,
+                        userName,
+                        userBirth,
+                        userPhone,
+                        userAddress
+                      )
+                    }
+                  >
+                    <h5>確定</h5>
+                    <GoTriangleRight
+                      style={{
+                        fontSize: "36px",
+                        color: "#eca061",
+                        border: "none"
+                      }}
+                    />
                   </div>
                 </>
-                )}
+              )}
             </div>
           </div>
         </head>
@@ -101,42 +175,79 @@ function MemberInfo() {
               <input
                 type="text"
                 placeholder={userEmail}
-                style={
-                  step ? {} : { backgroundColor: "#ccc" }
-                }
+                style={step ? {} : { backgroundColor: "#ccc" }}
                 disabled={!step}
-                name='userEmail'
-                onChange={step?(event)=>{changeValue(event)}:''}
+                name="userEmail"
+                onChange={
+                  step
+                    ? event => {
+                        changeValue(event);
+                      }
+                    : ""
+                }
               />
             </li>
             <li>
               <h5>會員名稱</h5>
-              <input type="text" 
-              placeholder={userName} 
-              style={step?{width: "160px" } : { backgroundColor: "#ccc", width: "160px" }} 
-              disabled={!step}
-              name='userName'
-              onChange={step?(event)=>{changeValue(event)}:''}
+              <input
+                type="text"
+                placeholder={userName}
+                style={
+                  step
+                    ? { width: "160px" }
+                    : { backgroundColor: "#ccc", width: "160px" }
+                }
+                disabled={!step}
+                name="userName"
+                onChange={
+                  step
+                    ? event => {
+                        changeValue(event);
+                      }
+                    : ""
+                }
               />
             </li>
             <li>
               <h5>會員生日</h5>
-              <input type="date" 
-              value={!step&&userBirth} 
-              style={step?{ width: "150px" }:{ backgroundColor: "#ccc", width: "150px"  }} 
-              disabled={!step}
-              name='userBirth'
-              onChange={step?(event)=>{changeValue(event)}:''}
+              <input
+                type="date"
+                value={userBirth}
+                style={
+                  step
+                    ? { width: "150px" }
+                    : { backgroundColor: "#ccc", width: "150px" }
+                }
+                disabled={!step}
+                name="userBirth"
+                onChange={
+                  step
+                    ? event => {
+                        changeValue(event);
+                      }
+                    : ""
+                }
               />
             </li>
             <li>
               <h5>會員電話</h5>
-              <input type="text" 
-              placeholder={userPhone} 
-              style={step?{ width: "180px" }:{ backgroundColor: "#ccc", width: "180px"  }} 
-              disabled={!step}
-              name='userPhone'
-              onChange={step?(event)=>{changeValue(event)}:''}
+              <input
+                type="text"
+                placeholder={userPhone}
+                style={
+                  step
+                    ? { width: "180px" }
+                    : { backgroundColor: "#ccc", width: "180px" }
+                }
+                disabled={!step}
+                name="userPhone"
+                onChange={
+                  step
+                    ? event => {
+                        changeValue(event);
+                      }
+                    : ""
+                }
               />
             </li>
             <li>
@@ -144,10 +255,20 @@ function MemberInfo() {
               <input
                 type="text"
                 placeholder={userAddress}
-                style={step?{ width: "280px" }:{ backgroundColor: "#ccc", width: "280px"  }}
+                style={
+                  step
+                    ? { width: "280px" }
+                    : { backgroundColor: "#ccc", width: "280px" }
+                }
                 disabled={!step}
-                name='userAddress'
-              onChange={step?(event)=>{changeValue(event)}:''}
+                name="userAddress"
+                onChange={
+                  step
+                    ? event => {
+                        changeValue(event);
+                      }
+                    : ""
+                }
               />
             </li>
           </ul>
